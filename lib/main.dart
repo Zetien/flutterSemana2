450f8cfcda5bb89fb5c1b0c9dac955802ed37058
Jorge Zetien <jzetien@lsv-tech.com>
2022-06-16 16:03:06 -0500

    Agregado splash con lottie y agregado menu

--- lib/main.dart
+++ lib/main.dart
@@ -2,8 +2,10 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_application_listas/views/listView1.dart';
 import 'package:flutter_application_listas/views/listView2.dart';
+import 'package:flutter_application_listas/views/splash.dart';
 
 import 'views/lista_firebase.dart';
+import 'views/menu.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -20,6 +22,6 @@ class MyApp extends StatelessWidget {
     return const MaterialApp(
         debugShowCheckedModeBanner: false,
         title: 'material app',
-        home: CallFirebase());
+        home: Splash());
   }
 }
